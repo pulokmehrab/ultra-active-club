1fc29c49a543792b71d0fb03da719c09539a2707
pulok <khandokarmeherab@gmail.com>
2022-09-30 02:11:38 +0600

    toast done

--- src/components/FitnessTime/FitnessTour.js
+++ src/components/FitnessTime/FitnessTour.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const FitnessTour = ({timeNeed, breakTime}) => {
     let prevValue = 0;
     for (const time of timeNeed){
         prevValue += parseInt([time]);
     }
+
+    const notify = () => toast("Activity done");
+
     return (
         <div className='card mt-4 text-center'>
             <div className='mt-3'>
@@ -15,7 +20,8 @@ const FitnessTour = ({timeNeed, breakTime}) => {
                 <h4>Break Time</h4>
                 <h4>{breakTime} s</h4>
             </div>
-            <button className='btn btn-primary'>Acitivity Check</button>
+            <button onClick={notify} className='btn btn-primary'>Acitivity Check</button>
+            <ToastContainer />
         </div>
     );
 };
